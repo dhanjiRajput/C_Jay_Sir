0faf7f165f31466d5c3c16ecb63d0a7986f88c09
dhanjiRajput <kidechadhanji@gmail.com>
2023-11-29 19:34:25 +0530

    29-11-2023

---
+++ 7.3/Q2.C
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i,j,k;
+	clrscr();
+	for(i=5;i>=1;i--)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ",j);
+		}
+		for(k=5;k>i;k--)
+		{
+			printf("    ");
+		}
+		for(j=i;j>=1;j--)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ",j);
+		}
+		for(k=5;k>i;k--)
+		{
+			printf("    ");
+		}
+		for(j=i;j>=1;j--)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+	getch();
+}
